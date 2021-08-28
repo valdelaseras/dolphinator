dfd06ae2aa016d29f926a39f1ac5b85782e81a9c
Robin Scholtes <robin@theweirdscience.com>
2021-08-28 17:06:38 +1200

    updated scripts, latin and japanese script now both possible

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+const nav = document.getElementById('nav');
+const navScriptBtns = nav.querySelectorAll('[data-script]');
+
 const clearTextareaBtn = document.getElementById('clear-textarea-btn');
 const submitFormBtn = document.getElementById('submit-form-btn');
 const swapDirectionBtn = document.getElementById('swap-direction-btn');
@@ -5,22 +8,18 @@ const swapDirectionBtn = document.getElementById('swap-direction-btn');
 const textArea = document.getElementById('textarea');
 const translationOutput = document.getElementById('translation-output');
 
-const defaultDolphinTextOutput = translationOutput.value = 'EeeEeEEEEeeEEeEeEeeEeeEEEeeEeeEEEeeEeeee EeEEeEEe EeeEEEEeEeeEeeEe EeeEEeEEEeeEeeeeEeeEeeEEEeeeEEEEEeeEeEEEEeeEeEEeEeeEeeeE';
-const defaultHumanTextOutput = translationOutput.value = 'Hello, I am dolphin';
-
-let selectedOutputLang = 'dolphin';
-let dialect = 'orca';
+// opposite option is 'human'
+let lang = 'dolphin';
+// other option is 'Japanese'
+let script = 'latin';
 let interval;
 
 window.onload = () => {
     displayConsoleArt();
+    updateTextareaPlaceholder();
 
     if ( screen.width >= 768 ){
-        if ( selectedOutputLang === 'dolphin' ){
-            displayTranslation( defaultDolphinTextOutput );
-        } else {
-            displayTranslation( defaultHumanTextOutput );
-        }
+        displayTranslation( translate( 'dolphin', 'latin', 'w e l c o m e' ));
     }
 }
 
@@ -32,65 +31,104 @@ const handleKeyUp = ( e ) => {
 };
 
 const eventHandler = ( e ) => {
-    e.target.blur();
-
     if ( e.target === clearTextareaBtn) {
         textArea.value = '';
+        textArea.focus();
+
     } else if ( e.target === submitFormBtn) {
-        translationOutput.innerText = '';
+        clearOutput();
+        e.target.blur();
+        displayTranslation( translate( lang, script, textArea.value) );
 
-        if (interval) {
-            clearInterval(interval);
-            interval = null;
-        }
-
-        const translation = translate(selectedOutputLang, dialect, textArea.value);
-        displayTranslation(translation);
     } else if (e.target === swapDirectionBtn || e.target === swapDirectionBtn.querySelector('img') ) {
-        toggleOutputLang();
-    }
+        swapTranslationDirection();
 
+    } else if ( e.target.hasAttribute('data-script')){
+        updateScript( e );
+    }
+}
+
+const updateScript = ( e ) => {
+    // handle script button classes
+    removeClassOnElements( 'selected', navScriptBtns );
+    e.target.classList.add('selected');
+
+    // set script to target script
+    script = e.target.getAttribute( 'data-script');
+
+    // update the textarea accordingly
+    updateTextareaPlaceholder();
+    textArea.value = '';
     textArea.focus();
+
+    clearOutput();
 }
 
 /*
-* Switch to translate in the intended language ( which is either ENG > Dolphin or
-* vice versa ). The dialects ( dolphin species ) have different en/decode methods,
-* resulting in different dolphin language outputs.
+* Clear the translation output field and any interval that may or may not be
+* running
 */
-const translate = ( lang, dialect, input ) => {
+const clearOutput = () => {
+    translationOutput.innerText = '';
+    if (interval) {
+        clearInterval(interval);
+        interval = null;
+    }
+}
+
+/*
+* Switch cases to handle the correct en/decoders based on the
+* selected lang ( human or dolphin ), script ( japanese or latin alphabets )
+* to translate the input
+*/
+const translate = ( lang, script, input ) => {
     let result;
     let charArray = input.split( '' );
 
-    if ( lang === 'dolphin') {
-        switch ( dialect ){
-            case 'orca':
-                result = encodeToOrca( charArray );
-                break;
+    if ( lang === 'human') {
+        switch( script ) {
+            case 'latin':
+                result = decodeFromLatinDolphin( charArray );
+                break
+            case 'japanese':
+                result = decodeFromJapaneseDolphin( charArray );
+                break
         }
-    } else if ( lang === 'english' ) {
-        switch ( dialect ) {
-            case 'orca':
-                result = decodeFromOrca( charArray );
-                break;
+    } else if ( lang === 'dolphin' ) {
+        switch( script ){
+            case 'latin':
+                result = encodeToLatinDolphin( charArray );
+                break
+            case 'japanese':
+                result = encodeToJapaneseDolphin( charArray );
+                break
         }
     }
 
     return result.join('');
 };
 
-/////////////////* START EN/DECODE *///////////////////
-/*
-* Encode English to binary
-*/
-const encodeToBinary = ( char ) => {
-    return char.charCodeAt(0).toString(2).padStart(8, '0');
-};
 
+
+
+/////////////////* START ENCODE TO BINARY *///////////////////
 /*
-* Decode orca dialect via binary to English
+* Encode latin alphabet to binary. Default padding is 8,
+* but Japanese chars for example need 24 padding
 */
-const decodeFromOrca = ( input ) => {
+const encodeToBinary = ( char, padding = 8 ) => {
+    return char.charCodeAt(0).toString(2).padStart( padding, '0');
+};
+/////////////////* END ENCODE TO BINARY *///////////////////
+
+
+
+
+/////////////////* START EN/DECODE LATIN ALPHABET *///////////////////
+/*
+* Decode Latin dolphin via binary to latin alphabet
+*/
+const decodeFromLatinDolphin = (input ) => {
     const binaryArray = [];
     for ( const char of input ) {
         if ( char === ' ' ) {
@@ -109,9 +147,9 @@ const decodeFromOrca = ( input ) => {
 };
 
 /*
-* Encode English to orca dialect via binary encoding
+* Encode latin alphabet to latin dolphin via binary encoding
 */
-const encodeToOrca = ( input ) => {
+const encodeToLatinDolphin = ( input ) => {
     return input.map( ( character ) => {
         if ( character === ' ' ) {
             return character;
@@ -121,7 +159,72 @@ const encodeToOrca = ( input ) => {
         }
     });
 };
-/////////////////* END EN/DECODE *///////////////////
+/////////////////* END EN/DECODE LATIN ALPHABET*///////////////////
+
+
+
+
+/////////////////* START EN/DECODE JAPANESE ALPHABET*///////////////////
+/*
+* Encode Japanese dolphin to human Japanese via binary decoding
+*/
+const decodeFromJapaneseDolphin = ( input ) => {
+    const binaryArray = [];
+    for ( const char of input ) {
+        binaryArray.push( char === 'エ' ? 1 : 0 );
+    }
+
+    const byteArray = [];
+    for ( let i = 0;  i < binaryArray.length; i += 24 ) {
+        byteArray.push(binaryArray.slice(i, i + 24).join(''))
+    }
+
+    return byteArray.map( byte => String.fromCharCode(parseInt( byte , 2 )) );
+}
+
+/*
+* Encode human Japanese to dolphin Japanese via binary encoding
+*/
+const encodeToJapaneseDolphin = ( input ) => {
+    return input.map( ( character ) => {
+        const binaryArray = encodeToBinary( character, 24 ).split('');
+        return binaryArray.map(( str ) => str === '1' ? 'エ' : 'え' ).join('');
+    });
+}
+/////////////////* END EN/DECODE JAPANESE ALPHABET*///////////////////
+
+
+
+
+/////////////////* START OUTPUT TOGGLE *///////////////////
+/*
+* Switch from Human to Dolphin and vice versa.
+*/
+const swapTranslationDirection = () => {
+    lang = lang === 'dolphin' ? 'human' : 'dolphin';
+    updateTextareaPlaceholder();
+    textArea.value = '';
+};
+
+const updateTextareaPlaceholder = ()  => {
+    if ( lang === 'dolphin' ) {
+        if ( script === 'japanese' ){
+            textArea.setAttribute('placeholder', 'テキストを入力してください。。。');
+        } else {
+            textArea.setAttribute( 'placeholder', 'Type something...');
+        }
+    } else {
+        if ( script === 'japanese' ){
+            textArea.setAttribute( 'placeholder', `${translate( 'dolphin', 'japanese', 'テキストを入力してください' )}。。。`);
+        } else {
+            textArea.setAttribute( 'placeholder', `${translate( 'dolphin', 'latin', 'Type something' )}...`);
+        }
+    }
+}
+/////////////////* END OUTPUT TOGGLE *///////////////////
+
+
+
 
 /////////////////* START DISPLAY *///////////////////
 /*
@@ -147,6 +250,15 @@ const displayTranslation = ( translation ) => {
         }
     }, 5);
 };
+
+/*
+* Remove a class on a collection of elements
+*/
+removeClassOnElements = ( className, elementArray ) => {
+    for ( let i = 0; i < elementArray.length; i++ ){
+        elementArray[i].classList.remove( className );
+    }
+}
 
 /*
  * Just display some extra dolphins in console :3
@@ -176,24 +288,6 @@ const displayConsoleArt = () => {
     );
 };
 /////////////////* END DISPLAY *///////////////////
-
-/////////////////* START OUTPUT LANG TOGGLE *///////////////////
-/*
-* Switch from English to Dolphin and vice versa.
-* The dialect indicates dolphin species and a different en/decoder method
-*/
-const toggleOutputLang = () => {
-    selectedOutputLang = selectedOutputLang === 'dolphin' ? 'english' : 'dolphin';
-
-    if ( selectedOutputLang === 'dolphin' ) {
-        textArea.setAttribute( 'placeholder', 'Type something...');
-    } else {
-        textArea.setAttribute( 'placeholder', `${translate( 'dolphin', dialect, 'Type something' )}...`);
-    }
-
-    textArea.value = '';
-};
-/////////////////* END OUTPUT LANG TOGGLE *///////////////////
 
 document.addEventListener('click', eventHandler );
 document.addEventListener('keydown', handleKeyUp );
